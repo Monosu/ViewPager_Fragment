4ed6f648984def0db72d95821ff5e373080d6a4a
linxi <767657043@qq.com>
2017-03-31 17:45:28 +0800

    company-2

---
+++ app/src/main/java/com/example/linxi/viewpager_fragment/base/BaseMenu.java
@@ -0,0 +1,19 @@
+package com.example.linxi.viewpager_fragment.base;
+
+import android.content.Context;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+
+/**
+ * Created by linxi on 2017/3/31.
+ */
+
+public abstract class BaseMenu {
+    private final String TAG="CustomMenu";
+    private Context mContext;
+
+    private PopupWindow mPopupWindow;
+    private ListView mListView;
+
+}
